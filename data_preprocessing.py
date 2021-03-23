64a678c5cd97d1613008a657f00bb5a4a0b7daa9
ardofski <ardagoktogan@gmail.com>
2021-03-23 23:52:17 +0300

    ascify and random words added

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -4,49 +4,77 @@ import math
 import random
 import numpy as np
 import sys
-
-
+import argparse
+from tqdm import tqdm
 
 class DataProcessing():
     
     def __init__(self):
-        self.keyboard_cartesian = {'q': {'x': 1, 'y': 1}, 'w': {'x': 2, 'y': 1}, 'e': {'x': 3, 'y': 1},
-                                   'r': {'x': 4, 'y': 1}, 't': {'x': 5, 'y': 1}, 'y': {'x': 6, 'y': 1},
-                                   'u': {'x': 7, 'y': 1}, 'ı': {'x': 8, 'y': 1}, 'o': {'x': 9, 'y': 1},
-                                   'p': {'x':10, 'y': 1}, 'ğ': {'x':11, 'y': 1}, 'ü': {'x':12, 'y': 1},
-                                   'a': {'x': 1, 'y': 2}, 's': {'x': 2, 'y': 2}, 'd': {'x': 3, 'y': 2},
-                                   'f': {'x': 4, 'y': 2}, 'g': {'x': 5, 'y': 2}, 'h': {'x': 6, 'y': 2},
-                                   'j': {'x': 7, 'y': 2}, 'k': {'x': 8, 'y': 2}, 'l': {'x': 9, 'y': 2},
-                                   'ş': {'x':10, 'y': 2}, 'i': {'x':11, 'y': 2},
-                                   'z': {'x': 1, 'y': 3}, 'x': {'x': 2, 'y': 3}, 'c': {'x': 3, 'y': 3},
-                                   'v': {'x': 4, 'y': 3}, 'b': {'x': 5, 'y': 3}, 'n': {'x': 6, 'y': 3},
-                                   'm': {'x': 7, 'y': 3}, 'ö': {'x': 8, 'y': 3}, 'ç': {'x': 9, 'y': 3}}
+
+        self.keyboard = [   ['q','w','e','r','t','y','u','ı','o','p','ğ','ü'],
+                            ['a','s','d','f','g','h','j','k','l','ş','i'],
+                            ['z','x','c','v','b','n','m','ö','ç'] ]
         
-        self.nearest_to_i = self.get_nearest_to_i()
+        self.flat_keyboard = []
+        for row in self.keyboard:
+            for c in row:
+                self.flat_keyboard.append(c)
+        
+        self.turkish_alphabet = 'abcçdefgğhıijklmnoöprsştuüvyzABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ'
+        self.ascii_turkish_alphabet = 'abccdefgghiijklmnooprsstuuvyzABCCDEFGGHIIJKLMNOOPRSSTUUVYZ'
+        self.ascii_mapping = {}
 
-    def get_nearest_to_i(self):
-        nearest_to_i = {}
-        for i in self.keyboard_cartesian.keys():
-            nearest_to_i[i] = []
-            for j in self.keyboard_cartesian.keys():
-                distance = self.euclidean_distance(i, j) 
-                if distance < 2.0:
-                    nearest_to_i[i].append(j)
-        return nearest_to_i
+        for i in range( len(self.turkish_alphabet) ):
+            self.ascii_mapping[ self.turkish_alphabet[i] ] = self.ascii_turkish_alphabet[i]
 
+        self.locations = self.get_locations()
+        self.replacement_matrix =  self.get_replacement_matrix()
 
-    def euclidean_distance(self, a, b):
-        dx = (self.keyboard_cartesian[a]['x'] - self.keyboard_cartesian[b]['x']) ** 2      
-        dy = (self.keyboard_cartesian[a]['y'] - self.keyboard_cartesian[b]['y']) ** 2
-        return math.sqrt(dx + dy)
+        self.corruption_methods = ["delete","insert","substitude","ascify"]
+        self.corruption_weights = [1,1,1,3]
+        self.generation_methods = ["foreign_lang","random"]
+        
+    def get_locations(self):
+        locations = {}
+        for i,row in enumerate(self.keyboard):
+            for j,col in enumerate( row):
+                locations[ self.keyboard[i][j] ] = (i,j)
 
-    def find_space(self, s, ch):
-        return [i for i, ltr in enumerate(s) if ltr == ch]
+        return locations
+        
+    def get_replacement_matrix( self ):
+
+        length = len( self.flat_keyboard )
+        distance_matrix = np.zeros( (length,length) )
+        
+        for i1,c1 in enumerate(self.flat_keyboard):
+            for i2,c2 in enumerate(self.flat_keyboard):
+                x1,y1 = self.locations[c1]
+                x2,y2 = self.locations[c2]
+                distance_matrix[i1][i2] = abs(x1-x2) + abs(y1-y2)
+
+        #define the weight for each distance probability
+        #ex. weights[1] = 10 and weights[2] = 2 means that distance=1 is 5 times more probable than distance=2
+        weights = [0,13,2,1]
+
+        replacement_matrix = distance_matrix
+        replacement_matrix[ replacement_matrix >= len(weights) ] = 0
+        
+        for i,w in enumerate(weights):
+            replacement_matrix[ distance_matrix==i ] = w
+
+        for row in replacement_matrix:
+            row /= np.sum(row)
+
+        return replacement_matrix
+
+    ## Following methods corrupt words or produce non-turkish words.
 
     def insert_char( self,  word ):
         word = [c for c in word]
         pos = random.randint(0, len(word) - 1)
-        insert_char = random.choice( self.nearest_to_i[ word[pos] ] )
+        c = word[pos]
+        insert_char = np.random.choice( self.flat_keyboard, p=self.replacement_matrix[self.flat_keyboard.index(c)] )
         word.insert( pos , insert_char )
         return ''.join(word)
 
@@ -59,57 +87,155 @@ class DataProcessing():
     def substitude_char( self, word ):
         word = [c for c in word]
         pos = random.randint(0, len(word) - 1 )
-        substitude_char = random.choice( self.nearest_to_i[ word[pos] ] )
+        c = word[pos]
+        substitude_char = np.random.choice( self.flat_keyboard, p=self.replacement_matrix[self.flat_keyboard.index(c)] )
         word[pos] = substitude_char
         return ''.join(word)
 
-    def corrupt_word(self, word, n = 5 ):
-        corrupted_words = []
-        for i in range(n):
+    def all_ascii( self, word):
+        return all(ord(c) < 128 for c in word)
+        '''
+        for c in word:
+            if c not in self.ascii_turkish_alphabet:
+                return False 
+        return True
+        '''
 
-            if len(word) > 1:
-                method = random.randint(1,3)
-            else:
-                method = random.randint(1,2)
-            if method == 1: #insert char
-                corrupted_word = self.insert_char( word )
-            elif method == 2: # delete char
-                corrupted_word = self.substitude_char( word )
-            else:
-                corrupted_word = self.delete_char( word )
+    def asciify_char( self, word ):
+        word = [c for c in word]
+        non_asciis = [ i  for i,c in enumerate(word) if ord(c) >= 128  ]
+        pos = random.choice( non_asciis )
+        #print("ascified word : ", word, " pos : " , pos)
+        substitude_char = random.choice( self.ascii_mapping[ word[pos] ] )
+        word[pos] = substitude_char
+        return ''.join(word)
 
-            corrupted_words.append( corrupted_word )
+    def random_word( self ):
+        length = random.choice( [4,5,6] )
+        chars = [ c for c in self.turkish_alphabet ]
+        word = [ random.choice(chars) for _ in range(length) ]
+        return ''.join(word)
 
-        return corrupted_words
+    def import_foreign_words(self, filename):
+        f = open( filename, "r")
+        lines = f.readlines()
+        lines = [l.rstrip() for l in lines]
+        lines = [l for l in lines if l.isalpha() and len(l) > 3 ]
+        self.foreign_words = lines
+        
+    def random_foreign_word( self ):
+        return random.choice( self.foreign_words )    
+
+    def random_corruption_method( self, word ):
+        methods = self.corruption_methods
+        weights = np.array( self.corruption_weights )
+        if len(word) == 1: #strings of lenth 1 should not be deleted
+            methods = methods[1:]
+            weights = weights[1:]
+        if self.all_ascii(word): #all ascii strings should not be asciified
+            methods = methods[:-1]
+            weights = weights[:-1]
+
+
+        weights = [w/np.sum(weights) for w in weights ]
+        method = np.random.choice( methods, p=weights )
+        return method
+
+
+    def corrupt_word(self, word ):
+        num_corruptions = 1
+        if len(word) > 4:
+            num_corruptions = np.random.choice( [1,2,3] , p = [0.7,0.25,0.05] )    
+
+        corrupted_word = word
+        for _ in range(num_corruptions):
+            method = self.random_corruption_method(corrupted_word)
+            if method == 'delete':
+                corrupted_word = self.delete_char( corrupted_word )
+            elif method == 'insert':
+                corrupted_word = self.insert_char( corrupted_word )
+            elif method == 'substitude':
+                corrupted_word = self.substitude_char( corrupted_word )
+            elif method == 'ascify':
+                corrupted_word = self.asciify_char( corrupted_word )
+
+        return corrupted_word
 
     def corrupt_words( self, words , n = 5 ):
-        corrupts = {}
+        corrupts = []
 
-        for word in words:
-            corrupts[ word ] = self.corrupt_word( word )
+        for word in tqdm(words):
+            corrupts.append( self.corrupt_word(word) )
 
         return corrupts
 
 '''
 Usage :
-$python <INPUT_FILE> <OUTPUT_FILE>
+$python --input-file <INPUT_FILE> --out-file <OUTPUT_FILE>
+$python --input-file <INPUT_FILE> --out-file <OUTPUT_FILE> --foreign-file <FOREIGN_WORDS_FILE>
 '''
+
 if __name__ == "__main__":
 
-    filename = sys.argv[1]
-    f = open(filename, 'r')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-file", type=str, required=True)
+    parser.add_argument("--out-file", type=str, required=True)
+    parser.add_argument("--foreign-file", type=str )
+    args = parser.parse_args()
+    
+    word_distirbution = {"random":0.05,"foreign_random":0.10,"corrupted":0.5,"non-corrupted":0.35}
+
+    f = open(args.input_file, 'r')
     lines = f.readlines()
     words = [word.rstrip() for word in lines ]
 
     processor = DataProcessing()
-    corrupted_words = processor.corrupt_words( words )
+    out_size = 10000
 
-    out_filename = sys.argv[2]
-    out_file = open( out_filename , "w")
-    
-    for key in corrupted_words.keys():
-        for cword in corrupted_words[key]:
-            out_file.write( key + ',' + cword + '\n' )
+    if args.foreign_file is not None:
+        processor.import_foreign_words(args.foreign_file)
+
+    #Corrupt given input list
+    #corrupted_words = processor.corrupt_words( words )
+
+    #corrupted words
+    ids = np.arange( len(words) )
+    corrupted_ids = np.random.choice(ids, int( out_size*word_distirbution['corrupted'] ),replace=False)
+
+    #non-corrupted words
+    non_corrupted_ids = np.random.choice(ids, int( out_size*word_distirbution['non-corrupted'] ),replace=False)
+
+    print("prepearing corrupted words...")
+    corrupted_words = [ processor.corrupt_word( words[i] ) for i in tqdm(corrupted_ids) ]
+
+    #Produce random words
+    print("prepearing random words...")
+    random_words = []
+    for _ in tqdm( range( int( out_size*word_distirbution['random'] ) ) ):
+        random_words.append( processor.random_word() )
+
+    #Produce random foreign words
+    random_foreign_words = []
+    if args.foreign_file is not None:
+        print("prepearing foreign words")
+        for _ in tqdm( range( int( out_size*word_distirbution['foreign_random'] ) ) ):
+            random_foreign_words.append( processor.random_foreign_word() )
+
+    #Write created words to file
+    out_file = open( args.out_file , "w")
+    out_file.write( 'gold,input\n' )
+
+    for i,id in enumerate(corrupted_ids):
+        out_file.write( words[id] + ',' + corrupted_words[i]  + '\n' )
+
+    for id in non_corrupted_ids:
+        out_file.write( words[id] + ',' + words[id] +  '\n' )
+
+    for word in random_foreign_words:
+        out_file.write( word + ',' + '\n' )
+
+    for word in random_words:
+        out_file.write( word + ',' + '\n' )
     
     out_file.close()
     
